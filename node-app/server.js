2285d7b89ae955eea80a82a101c55e1261fe2c08
Juan Gonzalez <juanpablogonzalez94@hotmail.com>
2025-03-07 17:47:26 -0300

    agregando cosas al tablero

--- node-app/server.js
+++ node-app/server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // URL de la aplicación de React
+    origin: ['http://localhost:3000', 'http://localhost'], // Permitir múltiples orígenes
     credentials: true
 }));
 app.use(session({
@@ -47,9 +47,11 @@ app.get('/api/data', (req, res) => {
         return res.status(401).json({ success: false, message: 'Unauthorized' });
     }
     const dataPath = path.join(__dirname, '../files/calculadora.txt');
+    const ltvPath = path.join(__dirname, '../files/ltv.json');
     try {
         const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-        res.json(data);
+        const ltv = JSON.parse(fs.readFileSync(ltvPath, 'utf8'));
+        res.json({ ...data, ltv });
     } catch (error) {
         console.error('Error reading data:', error);
         res.status(500).json({ success: false, message: 'Internal Server Error' });
@@ -61,15 +63,19 @@ app.post('/api/data', (req, res) => {
         return res.status(401).json({ success: false, message: 'Unauthorized' });
     }
     const dataPath = path.join(__dirname, '../files/calculadora.txt');
+    const ltvPath = path.join(__dirname, '../files/ltv.json');
     try {
         const existingData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        const existingLtv = JSON.parse(fs.readFileSync(ltvPath, 'utf8'));
         const newData = req.body;
 
         existingData.minAFinanciar = newData.minAFinanciar;
         const selectedProduct = Object.keys(newData.productos)[0];
         existingData.productos[selectedProduct] = newData.productos[selectedProduct];
+        existingLtv[selectedProduct] = newData.ltv[selectedProduct];
 
         fs.writeFileSync(dataPath, JSON.stringify(existingData, null, 2));
+        fs.writeFileSync(ltvPath, JSON.stringify(existingLtv, null, 2));
         res.json({ success: true });
     } catch (error) {
         console.error('Error saving data:', error);
